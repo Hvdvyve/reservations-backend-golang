b235e9c6537b76a2f3c88c0891f8a770ce405c71
Roel Spiessens <roel_spiessens@hotmail.com>
2021-12-20 02:07:18 +0000

    Added query functions for all golang functions. Fixed definitions for Consumable

--- Reservations/definitions.go
+++ Reservations/definitions.go
@@ -6,5 +6,31 @@ type Consumable struct {
 	Name      string `json:"Name"`
 	Type      string `json:"Type"`
 	Stock     string `json:"Stock"`
-	Available string `json:"False"`
+	Available string `json:"Available"`
+}
+
+type User struct {
+	Firstname string `json:"Firstname"`
+	Lastname  string `json:"Lastname"`
+	Email     string `json:"Email"`
+	Admin     string `json:"Admin"`
+}
+
+type Equipment struct {
+	Name        string `json:"Name"`
+	Description string `json:"Description"`
+}
+
+type Reservations struct {
+	From               string `json:"From"`
+	To                 string `json:"To"`
+	Rooms_and_desks_id string `json:"Rooms_and_desks_id"`
+}
+
+type RoomsAndDesks struct {
+	Wing   string `json:"Wing"`
+	Floor  string `json:"Floor"`
+	Number string `json:"Number"`
+	Places string `json:"Places"`
+	Type   string `json:"Type"`
 }
--- main.go
+++ main.go
@@ -22,10 +22,10 @@ import (
 	"log"
 	"sort"
 
+	reservation "github.com/gitpod/mycli/Reservations"
 	"github.com/gitpod/mycli/cmd"
 	"github.com/kr/pretty"
 	ravendb "github.com/ravendb/ravendb-go-client"
-	"github.com/gitpod/mycli/Reservations"
 )
 
 var (
@@ -48,9 +48,12 @@ var (
 
 func main() {
 	cmd.Execute()
-	getDocumentStore(dbName);
-	openSession(dbName);
-	queryCollectionByName()
+	getDocumentStore(dbName)
+	openSession(dbName)
+	queryGetConsumables()
+	queryGetRoomsAndDesks()
+	crudDeleteReservation("1")
+	crudAddConsumable("Chocolate", "Food", "20", "True")
 }
 
 func getDocumentStore(databaseName string) (*ravendb.DocumentStore, error) {
@@ -110,8 +113,7 @@ func printRQL(q *ravendb.DocumentQuery) {
 	fmt.Print("\n")
 }
 
-// query collection of a given name
-func queryCollectionByName() {
+func queryGetConsumables() {
 	store, session, err := openSession(dbName)
 	if err != nil {
 		log.Fatalf("openSession() failed with %s\n", err)
@@ -130,5 +132,167 @@ func queryCollectionByName() {
 	if len(results) > 0 {
 		fmt.Print("First result:\n")
 		pretty.Print(results[0])
+	}
+}
+
+// query collection of a given name
+func queryGetRoomsAndDesks() {
+	store, session, err := openSession(dbName)
+	if err != nil {
+		log.Fatalf("openSession() failed with %s\n", err)
+	}
+	defer store.Close()
+	defer session.Close()
+
+	q := session.QueryCollection("Consumables")
+	printRQL(q)
+
+	var results []*reservation.Consumable
+	err = q.GetResults(&results)
+	if err != nil {
+		log.Fatalf("q.GetResults() failed with '%s'\n", err)
+	}
+	if len(results) > 0 {
+		fmt.Print("First result:\n")
+		pretty.Print(results[0])
+	}
+}
+
+func crudDeleteReservation(reservationID string) {
+	store, err := getDocumentStore(dbName)
+	if err != nil {
+		log.Fatalf("getDocumentStore() failed with %s\n", err)
+	}
+	defer store.Close()
+
+	// delete the entity
+	{
+		session, err := store.OpenSession("")
+		if err != nil {
+			log.Fatalf("store.OpenSession() failed with %s\n", err)
+		}
+
+		var p *reservation.Reservations
+		err = session.Load(&p, reservationID)
+		if err != nil {
+			log.Fatalf("session.Load() failed with %s\n", err)
+		}
+
+		err = session.Delete(p)
+		if err != nil {
+			log.Fatalf("session.Delete() failed with %s\n", err)
+		}
+
+		err = session.SaveChanges()
+		if err != nil {
+			log.Fatalf("session.SaveChanges() failed with %s\n", err)
+		}
+
+		session.Close()
+	}
+
+	// verify entity was deleted
+	{
+		session, err := store.OpenSession("")
+		if err != nil {
+			log.Fatalf("store.OpenSession() failed with %s\n", err)
+		}
+
+		// try to load deleted entity from the server
+		var p *reservation.Reservations
+		err = session.Load(&p, reservationID)
+		if err != nil {
+			log.Fatalf("session.Load() failed with %s\n", err)
+		}
+
+		if p == nil {
+			fmt.Printf("Success: we deleted Reservation with this id so we get nil when we try to load it\n")
+		} else {
+			fmt.Printf("Error: this entity was deleted so we shouldn't be able to load it\n")
+		}
+
+		session.Close()
+	}
+}
+
+func crudDeleteConsumable(consumableID string) {
+	store, err := getDocumentStore(dbName)
+	if err != nil {
+		log.Fatalf("getDocumentStore() failed with %s\n", err)
+	}
+	defer store.Close()
+
+	// delete the entity
+	{
+		session, err := store.OpenSession("")
+		if err != nil {
+			log.Fatalf("store.OpenSession() failed with %s\n", err)
+		}
+
+		var p *reservation.Consumable
+		err = session.Load(&p, consumableID)
+		if err != nil {
+			log.Fatalf("session.Load() failed with %s\n", err)
+		}
+
+		err = session.Delete(p)
+		if err != nil {
+			log.Fatalf("session.Delete() failed with %s\n", err)
+		}
+
+		err = session.SaveChanges()
+		if err != nil {
+			log.Fatalf("session.SaveChanges() failed with %s\n", err)
+		}
+
+		session.Close()
+	}
+
+	// verify entity was deleted
+	{
+		session, err := store.OpenSession("")
+		if err != nil {
+			log.Fatalf("store.OpenSession() failed with %s\n", err)
+		}
+
+		// try to load deleted entity from the server
+		var p *reservation.Consumable
+		err = session.Load(&p, consumableID)
+		if err != nil {
+			log.Fatalf("session.Load() failed with %s\n", err)
+		}
+
+		if p == nil {
+			fmt.Printf("Success: we deleted Consumable with this id so we get nil when we try to load it\n")
+		} else {
+			fmt.Printf("Error: this entity was deleted so we shouldn't be able to load it\n")
+		}
+
+		session.Close()
+	}
+}
+
+func crudAddConsumable(Name string, Type string, Stock string, Available string) {
+	store, session, err := openSession(dbName)
+	if err != nil {
+		log.Fatalf("openSession() failed with %s\n", err)
+	}
+	defer store.Close()
+	defer session.Close()
+
+	consumable := &reservation.Consumable{
+		Name:      Name,
+		Type:      Type,
+		Stock:     Stock,
+		Available: Available,
+	}
+	err = session.Store(consumable)
+	if err != nil {
+		log.Fatalf("session.Store() failed with %s\n", err)
+	}
+	fmt.Printf("Consumable ID: %s\n", consumable.ID)
+	err = session.SaveChanges()
+	if err != nil {
+		log.Fatalf("session.SaveChanges() failed with %s\n", err)
 	}
 }
